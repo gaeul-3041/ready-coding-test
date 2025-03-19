b5c9d3dad9cb12dc64e091c322a474baa22215b0
Jin Hyun <pearl5322@gmail.com>
2025-03-19 20:40:43 +0900

    [level 2] Title: 튜플, Time: 33.09 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.py
+++ 프로그래머스/2/64065. 튜플/튜플.py
@@ -1,19 +1,13 @@
 from collections import defaultdict
 
 def solution(s):
-    count = defaultdict(int)
-    tup = s[2:-2].split('},{')
+    answer = defaultdict(int)
+    tup = sorted(s[2:-2].split('},{'), key=len)
     
     for t in tup:
         nums = t.split(',')
         for num in nums:
             n = int(num)
-            count[n] += 1
-    
-    count = sorted(count.items(), key=lambda x: x[1], reverse=True)
-    
-    answer = []
-    for c in count:
-        answer.append(c[0])
-    
-    return answer
+            answer[n] += 1
+
+    return list(answer)
